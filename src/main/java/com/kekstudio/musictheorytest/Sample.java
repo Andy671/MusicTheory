658ebef33f799b566a4e2fb3be5581252f1f0402
Andy671 <andreykotlaren@gmail.com>
2017-02-04 11:26:10 +0200

    Sample Update

--- src/main/java/com/kekstudio/musictheorytest/Sample.java
+++ src/main/java/com/kekstudio/musictheorytest/Sample.java
@@ -24,21 +24,22 @@ public class Sample {
         System.out.println(dSharpNote); // D#5[75]
         
         Chord dSharpm7Chord = dSharpNote.chord("m7"); 
-        System.out.println(dSharpm7Chord); //{D#5[75], F#5[78], A#5[82], C#6[85]}
+        System.out.println(dSharpm7Chord); // D#m7 inversion[0] {D#5[75], F#5[78], A#5[82], C#6[85]}
         
         Scale dSharpScale = dSharpNote.scale("minor");
-        System.out.println(dSharpScale); //{D#5[75], E#5[77], F#5[78], G#5[80], A#5[82], B5[83], C#6[85]}
+        System.out.println(dSharpScale); // D# minor scale {D#5[75], E#5[77], F#5[78], G#5[80], A#5[82], B5[83], C#6[85]}
         
         Key cFlatMajorKey = new Key("Cb", "major");
+        System.out.println(cFlatMajorKey); // Cb major key
         
         Chord thirdFlatDegreeAugChord = cFlatMajorKey.chord("bIII", "aug");
-        System.out.println(thirdFlatDegreeAugChord); //{Ebb4[62], Gb4[66], Bb4[70]}
+        System.out.println(thirdFlatDegreeAugChord); // Ebbaug inversion[0] {Ebb4[62], Gb4[66], Bb4[70]}
         
         thirdFlatDegreeAugChord.setPosition(1);
-        System.out.println(thirdFlatDegreeAugChord); //{Bb5[82], Ebb4[62], Gb4[66]}
+        System.out.println(thirdFlatDegreeAugChord); // Ebbaug inversion[1] {Bb5[82], Ebb4[62], Gb4[66]}
         
         thirdFlatDegreeAugChord.sort();
-        System.out.println(thirdFlatDegreeAugChord); //{Ebb4[62], Gb4[66], Bb5[82]}
+        System.out.println(thirdFlatDegreeAugChord); // Ebbaug inversion[1] {Ebb4[62], Gb4[66], Bb5[82]}
         
     }
    
